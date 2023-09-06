2a185f2c0671e9606ec3b014ed0b9ba8cf5ff1dd
MithunA12 <101501944+MithunA12@users.noreply.github.com>
2023-09-06 08:19:19 -0500

    started java and completed retrieving input text data

--- app/src/main/java/com/example/calculatorapparun/MainActivity.java
+++ app/src/main/java/com/example/calculatorapparun/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.calculatorapparun;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean isAdd;
+    boolean isSub;
+    boolean isMult;
+    boolean isDiv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void calculate(View v){
+
+        EditText val1ET = findViewById(R.id.inputTextOne);
+        String val1 = val1ET.getText().toString();
+        double val1Double = Double.parseDouble(val1);
+
+        EditText val2ET = findViewById(R.id.inputTextTwo);
+        String val2 = val2ET.getText().toString();
+
+        if(isAdd){
+
+        }
     }
 }
